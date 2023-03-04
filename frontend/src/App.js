a5d7eadd8603a0dda2be7198bdaac91700873d70
Dionne <mylife33@hotmail.com>
2023-03-04 14:29:24 -0600

    init server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="/"> amazona </a>
+        <Link to="/"> amazona </Link>
       </header>
-      <main>list of products</main>
+      <main>
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+      </main>
     </div>
   );
 }
